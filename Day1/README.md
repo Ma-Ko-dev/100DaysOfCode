884dd56668f9fc23fd39de25a8228432c826c40e
Ma-Ko-dev <Mathias.Kowitz@gmx.de>
2022-09-03 07:48:25 +0200

    Initial commit

---
+++ Day1/README.md
@@ -0,0 +1,4 @@
+# Day 1: Band Name Generator
+#100DaysOfCode
+
+It's day 1 of the 100 Days of Code Challenge and everything begins with some simple stuff! Let's go! 
---
+++ Day1/main.py
@@ -0,0 +1,7 @@
+print("Hello and welcome to the Band Name Generator 9000!")
+# asking for some input 2 times and putting the cursor in a new line with \n
+city_name = input("Tell me something about you! In what City did you grew up?\n")
+pet_name = input("Very interesting! What is your pets name?\n")
+# putting everyting together. Very simple and very stupid :D
+print("Alright! I think a good name for your Band would be: " + city_name + " " + pet_name + "!")
+# or simpler: print(f"Blabla {city_name} {pet_name}!)
