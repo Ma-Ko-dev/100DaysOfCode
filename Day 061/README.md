18b7db03e802352be3ae5f89d8e7582b12da7ff2
Ma-Ko-dev <Mathias.Kowitz@gmx.de>
2022-11-07 12:59:14 +0100

    Day 61 commit

---
+++ Day 061/README.md
@@ -0,0 +1,4 @@
+# Day 61:
+#100DaysOfCode
+
+It's day 61 of the 100 Days of Code Challenge! Today I learned about WTForms. Love the name! I practiced using WTForm by creating a basic form to "login" and see different sites depending on if the credentials where correct or not. I also learned about quick_form() and that's some serious magic working ;)
---
+++ Day 061/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class NewForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log in')
+
+
+app = Flask(__name__)
+app.secret_key = "thisissupersecure"
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = NewForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
